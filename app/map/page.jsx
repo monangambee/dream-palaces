e87e79064999084016f9baa0387f6b60259247e6
raytheboffin <111377374+josephchikak@users.noreply.github.com>
2025-11-20 18:39:26 +0100

    Update map page

--- app/map/page.jsx
+++ app/map/page.jsx
@@ -3,7 +3,7 @@
 export default function Map() { 
     return (
         <div className="w-screen min-h-screen h-screen bg-background text-primary">
-            <iframe width='100%' height='100%' src="https://dream-palace-web-map-omega.vercel.app/"></iframe>
+            <iframe width='100%' height='100%' src="https://dream-palaces-web-map.vercel.app/"></iframe>
         </div>
     );
 }
